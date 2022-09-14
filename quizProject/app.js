2a82d9f341434df137a1731c11f69d55917309ab
oliver-olive <65835019+oliver-olive@users.noreply.github.com>
2022-09-13 17:52:15 -0700

    add quiz js project

---
+++ quizProject/app.js
@@ -0,0 +1,34 @@
+const correctAnswers = ['B', 'B', 'B', 'B'];
+const form = document.querySelector('.quiz-form');
+const res = document.querySelector('.result');
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  let score = 0;
+  const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
+
+  // check the answers
+  userAnswers.forEach((answer, index) => {
+    if (answer === correctAnswers[index]){
+      score += 25;
+    }
+  });
+
+  // log the score to console
+  //console.log(score);
+  scrollTo(0,0);
+  res.querySelector('span').textContent = `${score}%`;
+  res.classList.remove('d-none');
+
+  let output = 0
+  const timer = setInterval( () => {
+    res.querySelector('span').textContent = `${output}%`;
+    if(output == score){
+        clearInterval(timer);
+    }else{
+        output ++;
+    }
+
+  }, 10);
+
+});
